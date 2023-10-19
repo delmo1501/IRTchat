e3ecfc24d5d6f0bc3e8d04f389ba8c7b7356ecde
Bruno <delmo123123@gmail.com>
2023-10-19 11:17:29 +0200

    websockets working

--- server/index.js
+++ server/index.js
@@ -1,15 +1,29 @@
 import express from 'express'
 import logger from 'morgan'
 
+import { Server } from 'socket.io'
+import { createServer } from 'node:http' 
+
 const port = process.env.PORT ?? 3000
 
 const app = express ()
+const server = createServer(app)
+const io = new Server(server)
+
+io.on('connection', (socket) => {
+    console.log('user connected!')
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected!')
+    })
+})
+
 app.use(logger('dev'))
 
 app.get('/', (req, res) => {
     res.sendFile(process.cwd() + '/client/index.html')
 })
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log('server running')
 })
