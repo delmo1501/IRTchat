f3b55b99d2fd4699be66611db48d09c33107c6f5
Bruno <delmo123123@gmail.com>
2023-10-21 16:02:19 +0200

    fixing turbo problem, db, and libsql

--- server/index.js
+++ server/index.js
@@ -1,20 +1,53 @@
 import express from 'express'
 import logger from 'morgan'
+import dotenv from 'dotenv'
+import { createClient } from '@libsql/client'
 
 import { Server } from 'socket.io'
-import { createServer } from 'node:http' 
+import { createServer } from 'http'
 
+dotenv.config()
 const port = process.env.PORT ?? 3000
 
 const app = express ()
 const server = createServer(app)
-const io = new Server(server)
+const io = new Server(server, {
+    connectionStateRecovery: {
+        maxDisconnectionDuration: {}
+    }
+})
+
+const db = createClient({
+    url: "libsql://genuine-savant-delmo1501.turso.io",
+    authToken: process.env.DB_TOKEN,
+})
+
+await db.execute(`
+    CREATE TABLE IF NOT EXISTS messages (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        message TEXT NOT NULL
+    )
+`)
 
 io.on('connection', (socket) => {
     console.log('user connected!')
 
     socket.on('disconnect', () => {
         console.log('user disconnected!')
+    })
+
+    socket.on('chat message', async (msg) => {
+        let result
+        try {
+            result = await db.execute({
+                sql: 'INSERT INTO messages (content) VALUES (:msg)',
+                args: { msg }
+            })
+        } catch (e) {
+          console.error(e)
+          return   
+        }
+        io.emit('chat message', msg, result.lastInsertRowid.toString())
     })
 })
 
