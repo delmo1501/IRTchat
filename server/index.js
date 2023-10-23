fbb83d26f45adb786fd90f90b832278905738b69
Bruno <delmo123123@gmail.com>
2023-10-23 11:44:16 +0200

    random user with message

--- server/index.js
+++ server/index.js
@@ -19,13 +19,14 @@ const io = new Server(server, {
 
 const db = createClient({
     url: "libsql://genuine-savant-delmo1501.turso.io",
-    authToken: process.env.DB_TOKEN,
+    authToken: process.env.DB_TOKEN
 })
 
 await db.execute(`
     CREATE TABLE IF NOT EXISTS messages (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        message TEXT NOT NULL
+        content TEXT,
+        user TEXT
     )
 `)
 
@@ -38,28 +39,30 @@ io.on('connection', async (socket) => {
 
     socket.on('chat message', async (msg) => {
         let result
+        let username = socket.handshake.auth.username ?? 'anonymous'
         try {
+
             result = await db.execute({
-                sql: 'INSERT INTO messages (content) VALUES (:msg)',
+                sql: 'INSERT INTO messages (content, user) VALUES (:msg, :username)',
                 // why we are not using `${msg}`? Like this we prevent random injections from the user
-                args: { msg }
+                args: { msg, username }
             })
         } catch (e) {
           console.error(e)
           return   
         }
-        io.emit('chat message', msg, result.lastInsertRowid.toString())
+        io.emit('chat message', msg, result.lastInsertRowid.toString(), username)
     })
 
     if (!socket.recovered) {
         try {
             const results = await db.execute({
-                sql: 'SELECT id, content FROM messages WHERE id > ?',
+                sql: 'SELECT id, content, user FROM messages WHERE id > ?',
                 args: [socket.handshake.auth.serverOffset ?? 0]
             })
 
             results.rows.forEach(row => {
-                socket.emit('chat message', row.content, row.id.toString())
+                socket.emit('chat message', row.content, row.id.toString(), row.username)
             })
         } catch (error) {
             console.error(error)
